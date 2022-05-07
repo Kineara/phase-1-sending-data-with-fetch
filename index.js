f8d206f73317a35e096e5847b8138946a2464ea4
Lauren Hoock <laurenh8916@protonmail.com>
2022-05-06 20:06:10 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(newName, newEmail) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: newName,
+      email: newEmail,
+    }),
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      const resElement = document.createElement("div");
+      resElement.textContent = json.id;
+      document.body.appendChild(resElement);
+    })
+    .catch(error => {
+      const errorElement = document.createElement("div");
+      errorElement.textContent = error.message;
+      document.body.appendChild(errorElement);
+    })
+}
